8b053a5b522ef9162e61f0fc3d7c0ca11336083a
bishoy <bishoy.saberibrahim@gmail.com>
2023-07-02 15:49:38 +0300

    initial commit

---
+++ Models/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Models
+{
+    public class ChatHub:Hub
+    {
+        public async Task sendMessage(string message,string name)
+        {
+            await Clients.All.SendAsync("messageTosend",message,name);
+
+
+        }
+    }
+}
